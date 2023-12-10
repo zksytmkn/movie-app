b03bc4a73dc6bec80ac1468e1644c0c08101c9a8
nakamoto <zksytmkn4869@gmail.com>
2023-12-10 14:43:31 +0900

    ✨

---
+++ actions/movie.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+});
+
+export const fetchMovies = async () => {
+  const { data: NetflixOriginal } = await instance.get(`/discover/tv?api_key=${process.env.API_KEY}&with_networks=213`);
+  const { data: Top } = await instance.get(`/discover/tv?api_key=${process.env.API_KEY}&languager=en-us`);
+  const { data: Action } = await instance.get(`/discover/tv?api_key=${process.env.API_KEY}&with_genres=28`);
+  const { data: Comedy } = await instance.get(`/discover/tv?api_key=${process.env.API_KEY}&with_genres=35`);
+  const { data: Horror } = await instance.get(`/discover/tv?api_key=${process.env.API_KEY}&with_genres=27`);
+  const { data: Romantic } = await instance.get(`/discover/tv?api_key=${process.env.API_KEY}&with_genres=10749`);
+  const { data: Documentary } = await instance.get(`/discover/tv?api_key=${process.env.API_KEY}&with_genres=99`);
+  return {
+    NetflixOriginal,
+    Top,
+    Action,
+    Comedy,
+    Horror,
+    Romantic,
+    Documentary,
+  };
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return;
+import { Row } from "../components/row";
+import { fetchMovies } from "@/actions/movie";
+
+export default async function Home() {
+  const data = await fetchMovies();
+
+  return (
+    <>
+      <Row title="Only on Netflix" movies={data.NetflixOriginal.results} isLargeRow />
+      <Row title="Global Top 20" movies={data.Top.results} />
+      <Row title="Action & Adventure" movies={data.Action.results} />
+      <Row title="Comedy" movies={data.Comedy.results} />
+      <Row title="Horror" movies={data.Horror.results} />
+      <Row title="Romantic" movies={data.Romantic.results} />
+      <Row title="Documentary" movies={data.Documentary.results} />
+    </>
+  );
 }
