0aa3cd4ebbdf775f57a27a2ff29455934cbcd4b5
nakamoto <zksytmkn4869@gmail.com>
2023-12-10 21:33:53 +0900

    ✨

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={clsx("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">{children}</main>
-            <footer className="w-full flex items-center justify-center py-3"></footer>
+            <main>{children}</main>
           </div>
         </Providers>
       </body>
---
+++ components/banner.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@nextui-org/react";
+import { fetchBanner } from "@/actions/movie";
+
+export const Banner = async () => {
+  const base_url = "https://image.tmdb.org/t/p/original";
+  const banner = await fetchBanner();
+
+  function truncate(str: any, n: number) {
+    if (str !== undefined) {
+      return str.length > n ? str?.substr(0, n - 1) + "..." : str;
+    }
+  }
+
+  return (
+    <header
+      className="text-white object-cover h-[460px] bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${base_url}${banner?.backdrop_path})`,
+      }}
+    >
+      <div className="ml-7 pt-36 h-44">
+        <h1 className="text-5xl font-extrabold mb-4">{banner?.title || banner?.name || banner?.orignal_name}</h1>
+        <div className="flex gap-1">
+          <Button variant="ghost" className="font-bold">
+            Play
+          </Button>
+          <Button variant="ghost" className="font-bold">
+            My List
+          </Button>
+        </div>
+        <h1 className="mt-4 max-w-sm font-bold">{truncate(banner?.overview, 160)}</h1>
+      </div>
+    </header>
+  );
+};
